3e4f4eefec633731ac91026657b59c2c7280749a
rscho <rschotanus@roc-dev.com>
2019-09-05 12:34:08 +0200

    hooftstuk 1t/m3

--- src/h03/StaadDiagram.java
+++ src/h03/StaadDiagram.java
@@ -11,10 +11,47 @@ public class StaadDiagram extends Applet {
     public void paint(Graphics g) {
         setBackground(Color.black);
         g.setColor(Color.yellow);
-        g.drawRect(200, 40, 25, 100);
+        g.setColor (Color.blue);
+        g.fillRect (200,40,25,200);
+        g.drawRect(200, 40, 25, 200);
+        g.setColor (Color.white);
+        g.fillRect (150,80,25,160);
+        g.drawRect(150, 80, 25, 160);
+        g.setColor (Color.red);
+        g.fillRect (100,160,25,80);
+        g.drawRect(100, 160, 25, 80);
+        g.setColor (Color.white);
+        g.drawOval(70, 40, 1, 200);
 
-        g.drawRect(150, 65, 25, 100);
+        g.drawOval(70, 240, 180, 1);
+        
+        g.drawString("1", 60, 245);
 
-        g.drawRect(100, 90, 25, 100);
+        g.drawString("100", 45, 45);
+
+        g.drawString("80", 50, 85);
+
+        g.drawString("60", 50, 125);
+
+        g.drawString("40", 50, 165);
+
+        g.drawString("20", 50, 205);
+
+        g.drawString("Valerie", 320, 70);
+
+        g.drawString("Hans", 320, 90);
+
+        g.drawString("Jeroen", 320, 110);
+        g.setColor(Color.RED);
+        g.fillRect (280,60,10,10);
+        g.drawRect(280, 60, 10, 10);
+        g.setColor(Color.WHITE);
+        g.fillRect (280,80,10,10);
+        g.drawRect(280, 80, 10, 10);
+        g.setColor(Color.BLUE);
+        g.fillRect (280,100,10,10);
+        g.drawRect(280, 100, 10, 10);
+        g.setColor(Color.WHITE);
+        g.drawRect(260, 50, 120, 70);
     }
 }
