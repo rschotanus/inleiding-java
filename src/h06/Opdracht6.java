d47d74870250a25e656b5d7ab144cb65134f3e52
Rogier <rschotanus@roc-dev.com>
2019-09-10 10:58:52 +0200

    opdracht 6.1 afgerond

--- src/h06/Opdracht6.java
+++ src/h06/Opdracht6.java
@@ -1,4 +1,24 @@
 package h06;
 
-public class Opdracht6 {
+import java.awt.*;
+import java.applet.*;
+
+
+public class Opdracht6 extends Applet {
+    int a;
+    int b;
+    int c;
+    int uitkomst;
+
+    public void init() {
+        a = 113;
+        b = 4;
+        c = 3;
+        uitkomst = (a / b);
+    }
+
+    public void paint(Graphics g) {
+        g.drawString("ieder krijgt: " + uitkomst, 20, 20);
+    }
 }
+
