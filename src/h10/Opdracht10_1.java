b6773f71c87a778171e9d5cfda5c22b84acb59a8
Rogier <rschotanus@roc-dev.com>
2019-09-20 08:34:17 +0200

    opdracht 10 afgemaakt

--- src/h10/Opdracht10_1.java
+++ src/h10/Opdracht10_1.java
@@ -1,4 +1,46 @@
 package h10;
 
-public class Opdracht10_1 {
+import java.awt.*;
+import java.applet.*;
+import java.awt.event.*;
+
+
+public class Opdracht10_1 extends Applet {
+
+    int leeftijd;
+    TextField tekstvak;
+    Label label;
+    String tekst;
+
+    public void init() {
+        tekstvak = new TextField("", 5);
+        tekstvak.addActionListener( new VakListener() );
+        tekst = "";
+
+        label = new Label("Geef uw leeftijd in en druk op enter" );
+
+        add( label );
+        add( tekstvak );
+    }
+
+    public void paint(Graphics g) {
+        g.drawString(tekst, 50, 45 );
+    }
+
+    class VakListener implements ActionListener {
+        public void actionPerformed( ActionEvent e ) {
+            String s;
+
+            s = tekstvak.getText();
+            leeftijd = Integer.parseInt( s );
+            if ( leeftijd > 20 ) {
+                tekst = "het getal is groter.";
+                repaint();
+            }
+            else {
+                tekst = "het getal is kleiner.";
+                repaint();
+            }
+        }
+    }
 }
