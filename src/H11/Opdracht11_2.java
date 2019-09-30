60b55d92c08bb18fdf329e4be2f7018b93bd75a0
Rogier <rschotanus@roc-dev.com>
2019-09-30 10:46:56 +0200

    afgemaakt

--- src/H11/Opdracht11_2.java
+++ src/H11/Opdracht11_2.java
@@ -1,4 +1,21 @@
 package H11;
 
-public class Opdracht11_2 {
+import java.awt.*;
+import java.applet.*;
+
+
+public class Opdracht11_2 extends Applet {
+
+    public void init() {}
+
+    public void paint(Graphics g) {
+        int teller = 20;
+        int y = 0;
+
+        while(teller > 9) {
+            y += 20;
+            g.drawString("" + teller, 305, y );
+            teller--;
+        }
+    }
 }
