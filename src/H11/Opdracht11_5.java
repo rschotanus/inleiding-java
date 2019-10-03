89d13100e53a4b72291ac2b809568acc82074748
Rogier <rschotanus@roc-dev.com>
2019-10-03 08:25:08 +0200

    klaar

--- src/H11/Opdracht11_5.java
+++ src/H11/Opdracht11_5.java
@@ -1,4 +1,23 @@
 package H11;
 
-public class Opdracht11_5 {
+import java.applet.Applet;
+import java.awt.*;
+
+public class Opdracht11_5 extends Applet{
+    int count,x,y, teller;
+    public void init() {
+        count = 5;
+        int y = 20;
+        int x = 20;
+
+    }
+    @Override
+    public void paint(Graphics g) {
+        for (teller = 1; teller <= count; teller++) {
+            y+= 20;
+            x+= 20;
+            g.drawRect(y,x,20,20);
+        }
+    }
+
 }
